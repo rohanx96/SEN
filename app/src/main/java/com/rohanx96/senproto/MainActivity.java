307c8fca0b24712042332f7081b5a031d0360bb6
srpanchal <srpanchal2712@gmail.com>
2016-03-27 01:32:24 +0530

    First commit

---
+++ app/src/main/java/com/rohanx96/senproto/MainActivity.java
@@ -0,0 +1,73 @@
+package com.rohanx96.senproto;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.track_order_container)
+    public void enterTripID(View view){
+        System.out.println("sda");
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setCustomTitle(LayoutInflater.from(this).inflate(R.layout.trip_id_dialog_heading, null));
+        alertDialog.setView(R.layout.trip_id_dialog);
+        alertDialog.create().show();
+    }
+
+    @OnClick(R.id.place_order_container)
+    public void placeOrder(){
+        Intent intent = new Intent(this,PlaceOrderActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.login)
+    public void login(){
+
+    }
+}
---
+++ app/src/main/java/com/rohanx96/senproto/PlaceOrderActivity.java
@@ -0,0 +1,219 @@
+package com.rohanx96.senproto;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.security.acl.Owner;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+import fr.ganfra.materialspinner.MaterialSpinner;
+
+/**
+ * Created by rose on 8/3/16.
+ */
+public class PlaceOrderActivity  extends AppCompatActivity implements OnConnectionFailedListener{
+
+
+    //TextView tvdetails1,tvdetails2;
+    private GoogleApiClient mGoogleApiClient;
+    String TAG="PlacesOrderActivity: ";
+    MaterialEditText etweight,etdate,etinfo,etcontact;
+    MaterialSpinner itemspinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_order);
+
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+
+
+        //tvdetails1=(TextView)findViewById(R.id.place_details1);
+        //tvdetails2=(TextView)findViewById(R.id.place_details2);
+
+        etweight=(MaterialEditText)findViewById(R.id.etweight);
+        etinfo=(MaterialEditText)findViewById(R.id.etinfo);
+        etcontact=(MaterialEditText)findViewById(R.id.etcontact);
+        itemspinner=(MaterialSpinner)findViewById(R.id.itemspinner);
+        etdate=(MaterialEditText)findViewById(R.id.etdate);
+        etdate.setKeyListener(null);
+
+        //entries for items' spinner
+        List<String> nameslist=new ArrayList<String>();
+        nameslist.add("Grains");
+        nameslist.add("Cotton");
+        nameslist.add("Tiles");
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, nameslist);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        itemspinner.setAdapter(adapter);
+
+
+        PlaceAutocompleteFragment from = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.autocomplete_fragment1);
+        from.setHint("From");
+        from.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Log.i("TAG", "Place Selected: " + place.getName());
+
+                // Format the returned place's details and display them in the TextView.
+                /*tvdetails1.setText(formatPlaceDetails(getResources(), place.getName(), place.getId(),
+                        place.getAddress(), place.getPhoneNumber(), place.getWebsiteUri()));
+
+                //Toast.makeText(MainActivity.this, place.getName(), Toast.LENGTH_SHORT).show();
+                CharSequence attributions = place.getAttributions();
+                if (!TextUtils.isEmpty(attributions)) {
+                    tvdetails1.setText(Html.fromHtml(attributions.toString()));
+                } else {
+                    //tvdetails.setText("Nothing");
+                }*/
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.e("TAG", "onError: Status = " + status.toString());
+
+                Toast.makeText(PlaceOrderActivity.this, "Place selection failed: " + status.getStatusMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        PlaceAutocompleteFragment to = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.autocomplete_fragment2);
+        to.setHint("To");
+        to.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Log.i("TAG", "Place Selected: " + place.getName());
+
+                // Format the returned place's details and display them in the TextView.
+                /*tvdetails2.setText(formatPlaceDetails(getResources(), place.getName(), place.getId(),
+                        place.getAddress(), place.getPhoneNumber(), place.getWebsiteUri()));
+
+                //Toast.makeText(MainActivity.this, place.getName(), Toast.LENGTH_SHORT).show();
+                CharSequence attributions = place.getAttributions();
+                if (!TextUtils.isEmpty(attributions)) {
+                    tvdetails2.setText(Html.fromHtml(attributions.toString()));
+                } else {
+                    //tvdetails.setText("Nothing");
+                }*/
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.e("TAG", "onError: Status = " + status.toString());
+
+                Toast.makeText(PlaceOrderActivity.this, "Place selection failed: " + status.getStatusMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        Calendar newCalendar = Calendar.getInstance();
+        final SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy", Locale.US);
+        final DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                etdate.setText(dateFormatter.format(newDate.getTime()));    //=================>date format
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        etdate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    datePickerDialog.show();
+                }
+            }
+        });
+        etdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
+
+    }
+
+
+
+
+
+
+    private static Spanned formatPlaceDetails(Resources res, CharSequence name, String id,
+                                              CharSequence address, CharSequence phoneNumber, Uri websiteUri) {
+        Log.e("TAG", res.getString(R.string.place_details, name, id, address, phoneNumber,
+                websiteUri));
+        return Html.fromHtml(res.getString(R.string.place_details, name, id, address, phoneNumber,
+                websiteUri));
+
+    }
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(PlaceOrderActivity.this, connectionResult.getErrorMessage(), Toast.LENGTH_SHORT).show();
+    }
+
+
+}
